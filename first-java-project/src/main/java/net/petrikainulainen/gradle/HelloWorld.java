2a86b266df6c25f689cf9d0df12b3cfc85c5b7b6
markg-intel <58740675+markg-intel@users.noreply.github.com>
2020-09-09 18:14:27 -0400

    Update HelloWorld.java

--- first-java-project/src/main/java/net/petrikainulainen/gradle/HelloWorld.java
+++ first-java-project/src/main/java/net/petrikainulainen/gradle/HelloWorld.java
@@ -6,6 +6,6 @@ package net.petrikainulainen.gradle;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Cruel World!");
     }
 }
